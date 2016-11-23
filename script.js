cddd12efd6b2cde862ab9bc218f569a3ba42deef
Biiz <bizzo174@gmail.com>
2016-11-23 21:48:18 +0100

    funzione addEmployee e retriveData

--- script.js
+++ script.js
@@ -15,6 +15,37 @@ function showHideForm() {
 }
 
 /**
+ * @brief Questa funzione spedisce e riceve un JSON, processandolo.
+ * @param [in] object form per processare i dati che contiene.
+ * @param [in] string option una stringa indicante l'azione da svolgere.
+ * @return il JSON processato.
+ */
+function retrieveData(form, option) {
+    // itera il form per collezionare gli input
+    var data = {};
+    for (var i = 0; i < form.length; i++) {
+        var input = form[i];
+        if (input.name) {
+            data[input.name] = input.value;
+        }
+    }
+    //set dell'opzione da eseguire
+    data['option']=option;
+    console.log(data);
+
+    //procedura di invio JSON
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.send(JSON.stringify(data));
+    //ritorno il JSON
+    return xhr;
+}
+
+//variabile globale che rappresenta il form sul quale stiamo agendo
+var form;
+
+/**
  * @brief Funzione che svuota i campi degli altri form, prende i valori dal form:
  * se almeno uno è mancante, segnala tramite un alert, altrimenti prepara il JSON
  * per spedirlo.
@@ -25,6 +56,29 @@ function addEmployee(){
 	//svuoto l'input del form di ricerca employee
 	document.getElementById('searchForm').reset();
 
+	// inizializzo il form
+    form = document.getElementById("addForm");
+    //creo e inizializzo i campi del form
+    var name = document.getElementById('name').value;
+    var surname = document.getElementById('surname').value;
+    var level = document.getElementById('level').value;
+    var salary = document.getElementById('salary').value;
+    
+    //se un campo è vuoto viene mostrato un alert
+    if (name == '' || surname == '' || level == '' || salary == '')
+        alert("manca almeno un input");
+    // altrimenti invoco la funzione per gestire il JSON
+    else {
+    	//spedisco il JSON invocando la procedura
+        xhr = retrieveData(form,"addEmployee");
+        //ricevo  il response del JSON
+        xhr.onloadend = function (){
+        	// responseText returns the body of the server's response as a string
+            var result=xhr.responseText;
+        };
+		//svuoto l'input del form di aggiunta employee       
+		document.getElementById("addForm").reset();
+    }
 }
 
 /**
