85e0f4c360a90332718a98215d327f103d6c0c15
Biiz <bizzo174@gmail.com>
2016-11-23 21:56:39 +0100

    corpo IF addEmployee

--- index.js
+++ index.js
@@ -57,6 +57,63 @@ app.use('/', function(request, response){
 
         //codice da eseguire se la scelta è di aggiungere un employee
 		if (option =="addEmployee") {
+            //verifico la presenza dell'ID
+            if ( typeof request.body.id !== 'undefined' && request.body.id) {
+                id = request.body.id;
+            }
+            //se l'ID non è stato inserito viene generato incrementando il più grande ID esistente
+            else {
+                var newId = 0;
+                //funzione per trovare il massimo ID presente
+                for (var i = 0; i < Object.keys(employees_list).length; i++) {
+                    if (employees_list[i].id >= newId)  newId= parseInt(employees_list[i].id)+1;  
+                }
+                //eguaglio l'ID dell'employee al massimo ID presente, maggiorato
+                id = newId;
+            }
+
+            //controlli per verificare la corretta presenza dei valori
+            if ( typeof request.body.name !== 'undefined' && request.body.name)
+                name = request.body.name;
+            if ( typeof request.body.surname !== 'undefined' && request.body.surname)
+                surname = request.body.surname;
+            if ( typeof request.body.level !== 'undefined' && request.body.level)
+                level = request.body.level;
+            if ( typeof request.body.salary !== 'undefined' && request.body.salary)
+                salary = request.body.salary;
+
+            //creo un nuovo employee che identifico come non presente
+            var employee = new Employee(id, name, surname, level, salary);
+            var alreadyIn = false;
+
+            //itero la lista degli impiegati per vedere se l'employee è già presente
+            for (var i = 0; i < Object.keys(employees_list).length; ++i) {
+                //se l'employee è presente, ne aggiorno i valori
+                if (employees_list[i].id == id){
+                    alreadyIn = true;
+                    employees_list[i].id= employee.id,
+                    employees_list[i].name= employee.name, 
+                    employees_list[i].surname= employee.surname, 
+                    employees_list[i].level= employee.level,
+                    employees_list[i].salary= employee.salary
+                    console.log("Employee aggiornato correttamente");
+                }
+            }
+            //se l'employee non è presente nella lista, lo aggiungo
+            if(!alreadyIn) {
+                employees_list.push(employee);
+            }
+            console.log("Numero di employees_list registrati: " + Object.keys(employees_list).length
+                       + ", employee appena aggiunto:");
+            //converto in una stringa JSON
+            json = JSON.stringify({ 
+                ID: employee.id,
+                name: employee.name, 
+                surname: employee.surname, 
+                level: employee.level,
+                salary: employee.salary
+            });
+            
         }
 
         //codice da eseguire se la scelta è di cercare un employee
